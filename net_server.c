39e8618f2b63d94e8c8d02de24c1d453ee51e81d
LiorBreitman8234 <liorbreitman321@gmail.com>
2022-07-14 10:50:39 +0300

    finished TCP-Programming part A, added needed code to net_client and comments to all 3 files, and a bit of explenantions in txt file

--- net_server.c
+++ net_server.c
@@ -1,43 +1,48 @@
 #include <sys/types.h>
-#include <unistd.h>          
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <signal.h>     
+#include <signal.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <netinet/in.h>
 
-#define PORT 9999
+#define PORT 10000
+#define SIM_LENGTH 10
 
-int main(void)
-{ 
-  int sock; 
-  int connect_sock; 
-  struct sockaddr_in serv_name; 
-  size_t len; 
-  int count;
-  
-  sock = socket(AF_INET, SOCK_STREAM, 0);  
-    
-  bzero(&serv_name, sizeof(serv_name)); 
-  serv_name.sin_family = AF_INET;                                   
-  serv_name.sin_port = htons(PORT); 
+int main(void) {
+    int sock;
+    int connect_sock;
+    struct sockaddr_in serv_name;
+    size_t len;
+    int count;
 
-  bind(sock, (struct sockaddr *)&serv_name, sizeof(serv_name));
-          
-  listen(sock, 1); 
+    sock = socket(AF_INET, SOCK_STREAM, 0); //creating TCP socket
 
-  len = sizeof(serv_name);
-  
-  connect_sock = accept(sock, (struct sockaddr *)&serv_name, &len);
+    //set up socket settings
+    bzero(&serv_name, sizeof(serv_name));
+    serv_name.sin_family = AF_INET;
+    serv_name.sin_port = htons(PORT);
 
-  for (count = 1; count <= SIM_LENGTH; count++)
-    { write(connect_sock, &count, 4);
-      printf("Server has written %d to socket.\n", count);
+    //binding socket with address
+    bind(sock, (struct sockaddr *) &serv_name, sizeof(serv_name));
+
+    //listening till 1 connection
+    listen(sock, 1);
+
+    len = sizeof(serv_name);
+
+    //accepting 1 connection
+    connect_sock = accept(sock, (struct sockaddr *) &serv_name, &len);
+
+    for (count = 1; count <= SIM_LENGTH; count++) {
+        write(connect_sock, &count, 4);//writing into socket
+        printf("Server has written %d to socket.\n", count);
     }
 
-  close(connect_sock); 
-  close(sock);
+    //closing sockets
+    close(connect_sock);
+    close(sock);
 
 }
--- nslookup.c
+++ nslookup.c
@@ -7,27 +7,28 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char* argv[]) {
-  struct addrinfo* res;
-  char* hostname;
-  char* hostaddr;
-  struct sockaddr_in* saddr;
-  
-  if (argc != 2) {
-    perror("Usage: hostnamelookup <hostname>\n");
-    exit(1);
-  }
+int main(int argc, char *argv[]) {
+    struct addrinfo *res;
+    char *hostname;
+    char *hostaddr;
+    struct sockaddr_in *saddr;
 
-  hostname = argv[1];
-  
-  if (0 != getaddrinfo(hostname, NULL, NULL, &res)) {
-    fprintf(stderr, "Error in resolving hostname %s\n", hostname);
-    exit(1);
-  }
-  
-  saddr = (struct sockaddr_in*)res->ai_addr;
-  hostaddr = inet_ntoa(saddr->sin_addr);
+    if (argc != 2) {
+        perror("Usage: hostnamelookup <hostname>\n");
+        exit(1);
+    }
 
-  printf("Address for %s is %s\n", hostname, hostaddr);
-  exit(0);
+    hostname = argv[1];//get commend line argument
+
+    if (0 != getaddrinfo(hostname, NULL, NULL, &res)) {//tries to copy the address of hostname to res
+        fprintf(stderr, "Error in resolving hostname %s\n", hostname);
+        exit(1);
+    }
+
+    //copying the address to a string that we can print
+    saddr = (struct sockaddr_in *) res->ai_addr;
+    hostaddr = inet_ntoa(saddr->sin_addr);
+
+    printf("Address for %s is %s\n", hostname, hostaddr);
+    exit(0);
 }
